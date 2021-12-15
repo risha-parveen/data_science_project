a525041b8a1eaf56f6f22874c570b337fdd45d0f
risha-parveen <rishaparveen12@gmail.com>
2021-12-15 21:45:03 +0530

    added

--- price_prediction/server/server.py
+++ price_prediction/server/server.py
@@ -1,12 +1,14 @@
-from flask import flask, request
-app=flask(__name__)
+from flask import Flask, request, jsonify
+app=Flask(__name__)
 
-@app.route('/hello')
-def hello():
+@app.route('/get_location_names')
+def get_location_names():
     return "hi"
 
 
 
+
 if __name__=="__main__":
-    print("starting python flask server for home price prediction")
-    app.run()
+    print("starting python flask server")
+    app.run(debug=True)
+    
