c3fe5effdeffdb1dcf7db521698adc6ae9035ff0
risha-parveen <rishaparveen12@gmail.com>
2021-12-17 20:37:45 +0530

    added

--- price_prediction/server/server.py
+++ price_prediction/server/server.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify
+import util
 app=Flask(__name__)
 
 @app.route('/get_location_names')
 def get_location_names():
-    return "hi"
+    response= jsonify({
+        'locations':util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 
 
---
+++ price_prediction/server/util.py
@@ -0,0 +1,5 @@
+
+
+def get_location_names():
+    pass
+
