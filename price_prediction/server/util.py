4c9a6286e6534db0f87999d82353b5a69710135f
risha-parveen <rishaparveen12@gmail.com>
2021-12-21 22:32:38 +0530

    added

--- price_prediction/server/util.py
+++ price_prediction/server/util.py
@@ -1,5 +1,28 @@
+import json
+import pickle
+__locations=None
+__data_columns=None
+__model=None
 
 
 def get_location_names():
-    pass
+    return __locations
 
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+    with open("/home/user/learning/data_science_project/price_prediction/server/artifacts/columns.json",'r') as f:
+        __data_columns=json.load(f)['data_columns']
+        __locations=__data_columns[3:]
+
+    with open("/home/user/learning/data_science_project/price_prediction/server/artifacts/banglore_home_prices_model.pickle",'rb') as f:
+        __model=pickle.load(f)
+    print("loading saved artifacts...done")
+
+
+if __name__=="__main__":
+    load_saved_artifacts()
+    print(get_location_names())
